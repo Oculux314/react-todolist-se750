e54bbe6a7c6965a048e7a122b211dd7765085956
Oculux <nate.williamson.nz@gmail.com>
2024-05-24 09:02:33 +1200

    :sparkles: create todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { app } from "./App.module.css";
+import TodoList from "./components/TodoList";
+import initialTodos from "./initialTodos";
+
 export default function App() {
-  return "Wow, it works!";
+  return (
+    <main className={app}>
+      <nav>
+        <h1>My Todo List</h1>
+        <TodoList todos={initialTodos} />
+      </nav>
+    </main>
+  );
 }
